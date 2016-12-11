0b075277c6c3671307817cbbbffc0a3e6f426735
Gibu George <gibu.george@cfcomputing.co.uk>
2016-12-11 13:01:23 +0530

    Added bootstrap listener and related changes to support bootstrap configurations

---
+++ platform-core/src/main/java/io/swiftwallet/platform/core/bootstrap/config/BootstrapConfiguration.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Nibodha Technologies Pvt. Ltd. 2016. All rights reserved.  http://www.nibodha.com
+ */
+
+package io.swiftwallet.platform.core.bootstrap.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author gibugeorge on 11/12/16.
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BootstrapConfiguration {
+}
