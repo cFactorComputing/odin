338e2aaebb92f96965329ba457d643ada4bacadd
Gibu George <gibu.george@swiftwallet.io>
2016-12-11 13:01:23 +0530

    Added bootstrap listener and related changes to support bootstrap configurations

---
+++ platform-core/src/main/java/io/swiftwallet/platform/core/bootstrap/ParentContextInitilizer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Nibodha Technologies Pvt. Ltd. 2016. All rights reserved.  http://www.nibodha.com
+ */
+
+package io.swiftwallet.platform.core.bootstrap;
+
+import org.springframework.boot.builder.ParentContextApplicationContextInitializer;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.Ordered;
+
+/**
+ * @author gibugeorge on 11/12/16.
+ * @version 1.0
+ */
+public class ParentContextInitilizer implements ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
+
+    private ConfigurableApplicationContext parentContext;
+
+    public ParentContextInitilizer(final ConfigurableApplicationContext parentContext) {
+        this.parentContext = parentContext;
+    }
+
+    @Override
+    public void initialize(final ConfigurableApplicationContext applicationContext) {
+        ConfigurableApplicationContext parentContext = null;
+        while (applicationContext.getParent() != null && applicationContext.getParent() != applicationContext) {
+            parentContext = (ConfigurableApplicationContext) applicationContext.getParent();
+        }
+        if (parentContext == null) {
+            parentContext = applicationContext;
+        }
+        new ParentContextApplicationContextInitializer(this.parentContext)
+                .initialize(parentContext);
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE + 1;
+    }
+
+    public void setParentContext(ConfigurableApplicationContext parentContext) {
+        this.parentContext = parentContext;
+    }
+}
