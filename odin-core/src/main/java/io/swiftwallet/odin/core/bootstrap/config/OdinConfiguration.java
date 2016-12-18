67d744870a36b00b55b3d879d89cb987e0ed5dd8
Gibu George <gibu.george@swiftwallet.io>
2016-12-18 20:00:41 +0530

    Changing the platform name to odin

---
+++ odin-core/src/main/java/io/swiftwallet/odin/core/bootstrap/config/OdinConfiguration.java
@@ -0,0 +1,20 @@
+
+
+package io.swiftwallet.odin.core.bootstrap.config;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author gibugeorge on 09/12/16.
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(OdinConfigurationImportSelector.class)
+public @interface OdinConfiguration {
+}
---
+++ odin-core/src/main/java/io/swiftwallet/odin/core/bootstrap/config/PropertySourceBootstrapConfiguration.java
@@ -0,0 +1,41 @@
+package io.swiftwallet.odin.core.bootstrap.config;
+
+import io.swiftwallet.odin.core.bootstrap.cd.ConfigurationPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * @author gibugeorge on 11/12/16.
+ * @version 1.0
+ */
+@Configuration
+public class PropertySourceBootstrapConfiguration implements ApplicationContextInitializer<ConfigurableApplicationContext>,Ordered {
+    private static final String BOOTSTRAP_PROPERTY_NAME = "bootstrapProperties";
+    private static final String BOOTSTRAP_PROPERTY_SOURCE_NAME="bootstrap.properties";
+
+    @Autowired
+    private PropertySourceLocator propertySourceLocator;
+
+
+    @Override
+    public void initialize(final ConfigurableApplicationContext applicationContext) {
+        final CompositePropertySource compositePropertySource = new CompositePropertySource(BOOTSTRAP_PROPERTY_SOURCE_NAME);
+        final PropertySource propertySource = propertySourceLocator.locate(applicationContext.getEnvironment());
+        compositePropertySource.addPropertySource(propertySource);
+        applicationContext.getEnvironment().getPropertySources().addFirst(compositePropertySource);
+        if (propertySource instanceof CompositePropertySource) {
+            applicationContext.getBeanFactory().registerSingleton("runtimeConfiguration", ((ConfigurationPropertySource) propertySource).getRuntimeConfiguration());
+        }
+
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE+1;
+    }
+}
