71e76a8606ef396e2172fde44c25c76c222683ae
Gibu George <gibu.george@cfcomputing.co.uk>
2016-12-18 20:19:35 +0530

    SWP-4:As a platform Odin should support HikariCP based JDBC Datasource configuration

---
+++ odin-core/src/main/java/io/swiftwallet/odin/core/service/jdbc/exception/DataSourceConfigurationException.java
@@ -0,0 +1,18 @@
+package io.swiftwallet.odin.core.service.jdbc.exception;
+
+import io.swiftwallet.odin.core.exception.OdinException;
+
+/**
+ * @author gibugeorge on 12/12/16.
+ * @version 1.0
+ */
+public class DataSourceConfigurationException extends OdinException {
+
+    public DataSourceConfigurationException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public DataSourceConfigurationException(String message) {
+        super(message);
+    }
+}
