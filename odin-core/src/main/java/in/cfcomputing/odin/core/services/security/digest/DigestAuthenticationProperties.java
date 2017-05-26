17be980ad74745860284ebc071cd99b5ce15e54a
Gibu George <gibu.george@cfcomputing.co.uk>
2017-05-26 14:23:25 +0530

    Changed to version 1.3-SNAPSHOT and moved to in.cfcomputing namespace

---
+++ odin-core/src/main/java/in/cfcomputing/odin/core/services/security/digest/DigestAuthenticationProperties.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 SwiftWallet Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package in.cfcomputing.odin.core.services.security.digest;
+
+import org.springframework.boot.autoconfigure.security.SecurityAuthorizeMode;
+
+/**
+ * Created by gibugeorge on 21/12/2016.
+ */
+public class DigestAuthenticationProperties {
+
+    private String realm = "Odin";
+    private SecurityAuthorizeMode authorizeMode = SecurityAuthorizeMode.ROLE;
+    private String[] path = new String[]{"/**"};
+    private String key = "uniqueAndSecret";
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public void setRealm(String realm) {
+        this.realm = realm;
+    }
+
+    public SecurityAuthorizeMode getAuthorizeMode() {
+        return authorizeMode;
+    }
+
+    public void setAuthorizeMode(SecurityAuthorizeMode authorizeMode) {
+        this.authorizeMode = authorizeMode;
+    }
+
+    public String[] getPath() {
+        return path;
+    }
+
+    public void setPath(String[] path) {
+        this.path = path;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
