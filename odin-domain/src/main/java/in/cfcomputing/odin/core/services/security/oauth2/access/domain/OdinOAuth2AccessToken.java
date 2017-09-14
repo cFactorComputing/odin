ab72692f208320d716a4327a6beeaa8cba5ffe2a
Anoop K John <anoop.kjohn@swiftwallet.io>
2017-09-14 21:13:23 +0530

    OAuth2 - storing tokens in Gemfire memory grid.

--- odin-domain/src/main/java/in/cfcomputing/odin/core/services/security/oauth2/access/domain/OdinOAuth2AccessToken.java
+++ odin-domain/src/main/java/in/cfcomputing/odin/core/services/security/oauth2/access/domain/OdinOAuth2AccessToken.java
@@ -15,9 +15,124 @@
 package in.cfcomputing.odin.core.services.security.oauth2.access.domain;
 
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 
-public class OdinOAuth2AccessToken extends DefaultOAuth2AccessToken {
-    public OdinOAuth2AccessToken() {
-        super((String) null);
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
+public class OdinOAuth2AccessToken {
+    private String value;
+    private Date expiration;
+    private String tokenType;
+    private OdinOAuth2RefreshToken refreshToken;
+    private Set<String> scope;
+    private Map<String, Object> additionalInformation;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public OdinOAuth2RefreshToken getRefreshToken() {
+        return refreshToken;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public void setRefreshToken(OdinOAuth2RefreshToken refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Set<String> getScope() {
+        return scope;
+    }
+
+    public void setScope(Set<String> scope) {
+        this.scope = scope;
+    }
+
+    public Map<String, Object> getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(Map<String, Object> additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
+    public OAuth2AccessToken toOAuth2AccessToken() {
+        final OAuth2AccessToken token = new OAuth2AccessToken() {
+            @Override
+            public Map<String, Object> getAdditionalInformation() {
+                return additionalInformation;
+            }
+
+            @Override
+            public Set<String> getScope() {
+                return scope;
+            }
+
+            @Override
+            public OAuth2RefreshToken getRefreshToken() {
+                return refreshToken != null ? refreshToken.toRefreshToken() : null;
+            }
+
+            @Override
+            public String getTokenType() {
+                return tokenType;
+            }
+
+            @Override
+            public boolean isExpired() {
+                return false;
+            }
+
+            @Override
+            public Date getExpiration() {
+                return expiration;
+            }
+
+            @Override
+            public int getExpiresIn() {
+                return 0;
+            }
+
+            @Override
+            public String getValue() {
+                return value;
+            }
+        };
+
+        return new DefaultOAuth2AccessToken(token);
+    }
+
+    public static OdinOAuth2AccessToken fromToken(final OAuth2AccessToken oAuth2AccessToken) {
+        final OdinOAuth2AccessToken odinOAuth2AccessToken = new OdinOAuth2AccessToken();
+        odinOAuth2AccessToken.setAdditionalInformation(oAuth2AccessToken.getAdditionalInformation());
+        odinOAuth2AccessToken.setExpiration(oAuth2AccessToken.getExpiration());
+        odinOAuth2AccessToken.setRefreshToken(OdinOAuth2RefreshToken.fromToken(oAuth2AccessToken.getRefreshToken()));
+        odinOAuth2AccessToken.setScope(oAuth2AccessToken.getScope());
+        odinOAuth2AccessToken.setTokenType(oAuth2AccessToken.getTokenType());
+        odinOAuth2AccessToken.setValue(oAuth2AccessToken.getValue());
+
+        return odinOAuth2AccessToken;
     }
 }
